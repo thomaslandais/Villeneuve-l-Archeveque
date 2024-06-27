268d461e806925b0a1b1acb63d09c0d0eb49781e
thomaslandais <129866484+thomaslandais@users.noreply.github.com>
2024-06-27 09:12:09 +0200

    add config files

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <WebView
+            source={{ uri: 'https://app.a2display.fr/login' }}
+            style={styles.webview}
+            originWhitelist={['https://app.a2display.fr/*']}
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+            scalesPageToFit={false}
+            injectedJavaScript={`
+              const meta = document.createElement('meta');
+              meta.name = 'viewport';
+              meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';
+              document.getElementsByTagName('head')[0].appendChild(meta);
+              const style = document.createElement('style');
+              style.innerHTML = 'body { overflow-x: hidden; }';
+              document.head.appendChild(style);
+            `}
+        />
+        <StatusBar style="auto" />
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    width: '100%',
+    overflow: 'hidden',
+  },
+  webview: {
+    flex: 1,
+    width: '100%',
   },
 });
---
+++ app-config.js
@@ -0,0 +1,8 @@
+export default config = {
+  // url: "",
+  // deviceId: 0,
+  apiUrl: `https://api.appli.a2display.fr`,
+  // apiUrl: `http://192.168.1.149:3006`,
+  apiRoot: `https://api.a2display.fr`,
+  // apiRoot: "http://192.168.1.149:8010"
+}
