8fd40f0b79ef4af181833fec6aad3ce657a025da
thomaslandais <129866484+thomaslandais@users.noreply.github.com>
2024-10-15 15:48:44 +0200

    villeneuve l'archeveque

--- App.js
+++ App.js
@@ -1,36 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, Modal, TouchableOpacity, Image } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { WebView } from 'react-native-webview';
 import Constants from 'expo-constants';
-import * as MediaLibrary from 'expo-media-library';
 import appConfig from './app-config';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
  // prod ou devLocal (changer ip si devLocal dans app-config.js)
  const uri = appConfig.prod;
- const [modalVisible, setModalVisible] = useState(false);
-
- useEffect(() => {
-  checkPermissions();
- }, []);
-
- const checkPermissions = async () => {
-  const { status: readStatus } = await MediaLibrary.getPermissionsAsync(false);
-  const { status: writeStatus } = await MediaLibrary.getPermissionsAsync(true);
-  const permissionAsked = await AsyncStorage.getItem('permissionAsked');
-  if ((readStatus !== 'granted' || writeStatus !== 'granted') && !permissionAsked) {
-   setModalVisible(true);
-   await AsyncStorage.setItem('permissionAsked', 'true');
-  }
- };
-
- const requestPermissions = async () => {
-  const { status: readStatus } = await MediaLibrary.requestPermissionsAsync(false);
-  const { status: writeStatus } = await MediaLibrary.requestPermissionsAsync(true);
-  setModalVisible(false);
- };
 
  const handleShouldStartLoadWithRequest = (request) => {
   const { url } = request;
@@ -51,28 +28,6 @@ export default function App() {
 
  return (
    <View style={styles.container}>
-    <Modal
-      animationType="slide"
-      transparent={true}
-      visible={modalVisible}
-      onRequestClose={() => {
-       setModalVisible(!modalVisible);
-      }}
-    >
-     <View style={styles.modalView}>
-      <Text style={styles.modalText}>
-       A2Box nécessite l'accès à vos photos et fichiers PDF pour que vous puissiez les sélectionner et les partager sur vos différents écrans via l'application. Cette autorisation est nécessaire pour que vous puissiez partager des fichiers depuis votre téléphone ou votre tablette connectés à votre compte A2Box.
-      </Text>
-      <Image source={require('./assets/icon.png')} style={styles.image} />
-      <TouchableOpacity
-        style={[styles.button, styles.buttonClose]}
-        onPress={requestPermissions}
-      >
-       <Text style={styles.textStyle}>Continuer</Text>
-      </TouchableOpacity>
-     </View>
-    </Modal>
-
     <WebView
       source={{ uri: uri }}
       style={styles.webview}
@@ -82,16 +37,15 @@ export default function App() {
       scalesPageToFit={false}
       onShouldStartLoadWithRequest={handleShouldStartLoadWithRequest}
       injectedJavaScript={`
-            const meta = document.createElement('meta');
-            meta.name = 'viewport';
-            meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';
-            document.getElementsByTagName('head')[0].appendChild(meta);
-            const style = document.createElement('style');
-            style.innerHTML = 'body { overflow-x: hidden; }';
-            document.head.appendChild(style);
-          `}
+          const meta = document.createElement('meta');
+          meta.name = 'viewport';
+          meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';
+          document.getElementsByTagName('head')[0].appendChild(meta);
+          const style = document.createElement('style');
+          style.innerHTML = 'body { overflow-x: hidden; }';
+          document.head.appendChild(style);
+        `}
     />
-
     <StatusBar style="auto" />
    </View>
  );
@@ -108,44 +62,4 @@ const styles = StyleSheet.create({
   flex: 1,
   width: '100%',
  },
- modalView: {
-  marginTop: "auto",
-  marginBottom: "auto",
-  margin: 20,
-  backgroundColor: 'white',
-  borderRadius: 20,
-  padding: 35,
-  alignItems: 'center',
-  shadowColor: '#000',
-  shadowOffset: {
-   width: 0,
-   height: 2
-  },
-  shadowOpacity: 0.25,
-  shadowRadius: 4,
-  elevation: 5
- },
- button: {
-  borderRadius: 20,
-  padding: 10,
-  elevation: 2
- },
- buttonClose: {
-  backgroundColor: '#2196F3',
- },
- textStyle: {
-  color: 'white',
-  fontWeight: 'bold',
-  textAlign: 'center',
-  margin: 5
- },
- modalText: {
-  marginBottom: 15,
-  textAlign: 'center'
- },
- image: {
-  width: 80,
-  height: 80,
-  padding: 15
- }
 });
--- app-config.js
+++ app-config.js
@@ -1,7 +1,7 @@
 export default config = {
   // url: "",
   // deviceId: 0,
-  prod: `https://app.a2display.fr/`,
+  prod: `https://stream.a2display.fr/578XUspAv3FOCVhIagfz4K1S`,
   // apiUrl: `http://192.168.1.149:3006`,
   devLocal: `192.168.1.78:3001/`,
   // apiRoot: "http://192.168.1.149:8010"
